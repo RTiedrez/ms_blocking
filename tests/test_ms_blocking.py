af8ca8ec6fd153e16f50d5b26eb82f11def588da
RTiedrez <pokroor@gmail.com>
2026-01-27 11:50:18 +0100

    test: add tests

--- tests/test_ms_blocking.py
+++ tests/test_ms_blocking.py
@@ -97,6 +97,21 @@ def attribute_city_motives_true_add():
     return [{"Same ['City']"}] * 9
 
 
+@pytest.fixture
+def attribute_city_show_as_pairs_true_id():
+    return {(1,4),(2,5),(3,11),(8,3),(8,11),(10,13)}
+
+
+@pytest.fixture
+def attribute_city_show_as_pairs_true_columns():
+    return 	["id_l", "Name_l", "id_r", "Name_r", "block"]
+
+
+@pytest.fixture
+def city_age_name_websites_pipelining_id():
+    return 	[1,4,2,5,8,11]
+
+
 def test_simple_attribute_equivalence_blocking(attribute_city_links):
     """Test that AB blocking does work as intended"""
     expected = attribute_city_links
@@ -226,4 +241,35 @@ def test_motives_when_adding_to_dataframe(attribute_city_motives_true_add):
     actual = msb.add_blocks_to_dataset(get_users(), links, motives=True)[
         "motive"
     ].to_list()
+    assert actual == expected
+
+def test_show_as_pairs(attribute_city_show_as_pairs_true_id):
+    """Test that test_show_as_pairs=True does work as intended"""
+    expected = attribute_city_show_as_pairs_true_id
+    city_blocker = msb.AttributeEquivalenceBlocker(["City"])
+    links = city_blocker.block(get_users(), motives=True)
+    blocked_df = msb.add_blocks_to_dataset(get_users(), links, show_as_pairs=True)
+    id_ls = blocked_df["id_l"].to_list()
+    id_rs = blocked_df["id_r"].to_list()
+    actual = set(zip(id_ls, id_rs))
+    assert actual == expected
+
+def test_output_columns(attribute_city_show_as_pairs_true_columns):
+    """Test that test_show_as_pairs=True does work as intended"""
+    expected = attribute_city_show_as_pairs_true_columns
+    city_blocker = msb.AttributeEquivalenceBlocker(["City"])
+    links = city_blocker.block(get_users(), motives=True)
+    actual = msb.add_blocks_to_dataset(get_users(), links, show_as_pairs=True, output_columns=["id", "Name"]).columns.to_list()
+    assert actual == expected
+
+def test_pipelining(city_age_name_websites_pipelining_id):
+    """Test that pipelining does work as intended"""
+    expected = city_age_name_websites_pipelining_id
+    city_blocker = msb.AttributeEquivalenceBlocker(["City"])
+    age_blocker = msb.AttributeEquivalenceBlocker(["Age"])
+    name_blocker = msb.AttributeEquivalenceBlocker(["Name"])
+    websites_blocker = msb.OverlapBlocker(["websites"])
+    final_blocker = (city_blocker & age_blocker) | (name_blocker & websites_blocker)
+    links = final_blocker.block(get_users(), motives=True)
+    actual = msb.add_blocks_to_dataset(get_users(), links)["id"].to_list()
     assert actual == expected
